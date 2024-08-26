c00410a56c4048390ddff1dc789d614a5a446f38
theparadox07 <theunknownpradox07@gmail.com>
2024-08-26 23:25:37 +0530

    navbar

--- app.py
+++ app.py
@@ -3,15 +3,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def caesar_decrypt(ciphertext, shift):
-    plaintext = ""
-    for char in ciphertext:
-        if char.isalpha():
-            offset = 65 if char.isupper() else 97
-            decrypted_char = chr((ord(char) - offset - shift) % 26 + offset)
-            plaintext += decrypted_char
-        else:
-            plaintext += char
-    return plaintext
+    # Caesar Cipher Decryption Logic
+    ...
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -25,6 +18,22 @@ def index():
             shift = int(shift)
             decrypted_message = caesar_decrypt(ciphertext, shift)
     return render_template("index.html", ciphertext=ciphertext, shift=shift, decrypted_message=decrypted_message)
+
+@app.route("/substitution_cipher")
+def substitution_cipher():
+    return render_template("substitution_cipher.html")
+
+@app.route("/rsa")
+def rsa():
+    return render_template("rsa.html")
+
+@app.route("/md5")
+def md5():
+    return render_template("md5.html")
+
+@app.route("/sha")
+def sha():
+    return render_template("sha.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
